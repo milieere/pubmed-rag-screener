e2e94177e5aa7cba53f0cae45c2a6be8e6c2f3d1
Barankova Simona CHBS <s1155157@CHBSL92HHTQ3.EAME.SYNGENTA.ORG>
2024-06-30 22:18:14 +0200

    minor wording / spaces changes

--- app/app.py
+++ app/app.py
@@ -10,26 +10,27 @@ def main():
         layout='wide'
     )
 
-    # Define two columns with the same width
+    # Define two columns - this will make layout split horizontally
     col1, col2 = st.columns([1, 3])
 
-    # In the first column, display an image
+    # Place the logo in the first column
     with col1:
         st.image('../assets/pubmed-screener-logo.jpg')
 
-    # In the second column, place text and interactive elements
+    # In the second column, place text explaining the purpose of the app and some example scientific questions that your user might ask.
     with col2:
         st.title("PubMed Screener")
         st.markdown("""
             PubMed Screener is a ChatGPT & PubMed powered insight generator from biomedical abstracts. 
 
-            #### Example prompts 'describe what are you looking for'
+            #### Example scientific questions
             - "I'm interested in research on the use of CRISPR technology for enhancing gene therapy in neurodegenerative diseases."
             - "I want to find articles discussing the role of gut microbiota in modulating immune responses in autoimmune disorders."
             - "I need information on the development of novel drug delivery systems using nanotechnology for targeted cancer therapy."
             - "What is the current understanding of the genetic factors influencing susceptibility to infectious diseases in pediatric populations?"
         """)
 
+    # This is the chatbot component
     chat_agent = ChatAgent(prompt=chat_prompt_template, llm=llm)
     chat_agent.start_conversation()
 
--- app/components/llm.py
+++ app/components/llm.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 AZURE_OPENAI_API_KEY = os.environ.get('AZURE_OPENAI_API_KEY')
 AZURE_OPENAI_DEPLOYMENT_NAME = os.environ.get('AZURE_OPENAI_DEPLOYMENT_NAME')
 AZURE_ENDPOINT = os.environ.get('AZURE_ENDPOINT')
 API_VERSION = os.environ.get('API_VERSION_LLM')
-
 
 llm = AzureChatOpenAI(
     azure_endpoint=AZURE_ENDPOINT,
